bc8e877abcab5a3432217b98f87c2cb740ed5c4d
CutePotatoDev <2453573+CutePotatoDev@users.noreply.github.com>
2022-07-18 22:06:59 +0300

    Some page fill up.

--- build.py
+++ build.py
@@ -19,18 +19,41 @@ class TemplateRender:
         return template.render(*args, **kwargs)
 
 
+def getFiles(dir):
+    files = []
+    for file in os.listdir(dir):
+        if os.path.isfile(os.path.join(dir, file)) and file.endswith("html"):
+            files.append(file)
+    return files
+
+def getDirs(dir):
+    dirs = []
+    for file in os.listdir(dir):
+        if not os.path.isfile(os.path.join(dir, file)):
+            dirs.append(file)
+    return dirs
+
 def main():
     path = "./view"
+
     render = TemplateRender(path=path)
 
-    for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)) and file.endswith("html"):
-            print("Build: `%s`." % file)
-            data = render.render(file)
+    for file in getFiles(path):
+        print("Build: `%s`." % os.path.join(path, file).replace("\\", "/"))
+        data = render.render(file)
 
-            with open(file, "w", encoding="UTF-8") as fd:
-                fd.write(data)
+        with open(file, "w", encoding="UTF-8") as fd:
+            fd.write(data)
 
+
+    for dir in getDirs(path):
+        if dir != "layout":
+            for file in getFiles(os.path.join(path, dir)):
+                print("Build: `%s`." % os.path.join(path, dir, file).replace("\\", "/"))
+                data = render.render(os.path.join(dir, file).replace("\\", "/"))
+
+                with open(os.path.join(dir, file), "w", encoding="UTF-8") as fd:
+                    fd.write(data)
 
 if __name__ == "__main__":
     main()
